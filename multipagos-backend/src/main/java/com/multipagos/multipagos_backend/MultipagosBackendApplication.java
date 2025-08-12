4262d80b952fa7f0b6505f6f47e9db83c728dde5
Jorge Lombana <lombanauci@gmail.com>
2025-08-11 23:21:34 -0500

    feat: init Spring Boot project
    - Spring Boot + Java
    - Maven configuration and wrapper
    - Basic project structure and application properties
    - Git configuration files

---
+++ multipagos-backend/src/main/java/com/multipagos/multipagos_backend/MultipagosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.multipagos.multipagos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipagosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipagosBackendApplication.class, args);
+	}
+
+}
---
+++ multipagos-backend/src/test/java/com/multipagos/multipagos_backend/MultipagosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multipagos.multipagos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipagosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
